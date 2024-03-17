06ca46b8c46518e92ae1e8a34fe7751ef5dbd16a
a.prakaptsova <a.prakaptsova@godeltech.com>
2024-03-17 22:22:07 +0300

    Proxy task 13: sentry logging has been added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const boolParser = require('express-query-boolean');
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 const config = require('./config/config');
 const asteroidRouter = require('./routes/asteroidsRouter');
 const photoRouter = require('./routes/roverPhotoRouter');
@@ -8,10 +10,24 @@ const validateQuery = require('./middleware/validator');
 const schemas = require('./validators/schemas/schema');
 const app = express();
 
+Sentry.init({
+    dsn: config.sentryDsn,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Sentry.Integrations.Express({ app }),
+      nodeProfilingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
 app.use(boolParser());
 app.use('/meteors', validateQuery(schemas.asteroidsSchema, 'query'), asteroidRouter);
 app.use('/photo', validateQuery(schemas.roverPhotoSchema, 'body'), photoRouter);
+app.use(Sentry.Handlers.errorHandler());
 app.use(errorLogger);
 app.use(exceptionHandler);
 app.use('*', pageNotFoundHandler);
